1f27ba5b4fac99958038a24e156a66b73bc6e8cc
alexisP73 <alexis.pretti@etu.univ-grenoble-alpes.fr>
2025-12-24 18:31:14 +0100

    resoudre erreur : 5

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -97,14 +97,13 @@ resource "azurerm_lb_probe" "http" {
 }
 
 resource "azurerm_lb_rule" "http" {
-  resource_group_name            = azurerm_resource_group.main.name
   loadbalancer_id                = azurerm_lb.main.id
   name                           = "http-rule"
   protocol                       = "Tcp"
   frontend_port                  = 80
   backend_port                   = 80
   frontend_ip_configuration_name = "public"
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.web.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.web.id]
   probe_id                       = azurerm_lb_probe.http.id
 }
 
